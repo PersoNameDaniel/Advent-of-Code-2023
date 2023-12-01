da0a28d55e15584f5b61c17020af25721906d762
PersoNameDaniel <dmolsson@outlook.com>
2023-12-01 08:34:26 -0600

    Add project files.

---
+++ Advent of Code 2023/day1Solution.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+// Read from a text file
+string path = @"C:\example.txt";
+string[] lines = File.ReadAllLines(path);
+foreach (string line in lines)
+{
+    Console.WriteLine(line);
+}
